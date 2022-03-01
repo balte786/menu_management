6327dec8244db785fbab139a2c20c6164e05ce25
iqbalbalte <iqbalbalte786@gmail.com>
2022-03-01 16:34:50 +0500

    latest updated files

--- resources/views/restorants/partials/modals.blade.php
+++ resources/views/restorants/partials/modals.blade.php
@@ -29,38 +29,29 @@
                                     <div id="exrtas-area-inside">
                                     </div>
                                 </div>
-                               @if(  !(isset($canDoOrdering)&&!$canDoOrdering)   )
+                                @if( !(isset($canDoOrdering)&&!$canDoOrdering) )
                                 <div class="quantity-area">
+
                                     <div class="form-group">
                                         <br />
-                                        <label class="form-control-label" for="quantity">{{ __('Quantity') }}</label>
+                                        <!--<label class="form-control-label" for="quantity">{{ __('Quantity') }}</label>-->
                                         <!--<input type="number" name="quantity" id="quantity" class="form-control form-control-alternative" placeholder="1" value="1" required autofocus>-->
-                                            <input
-                                                    type="number"
-                                                    min="1"
-                                                    step="1"
-                                                    onkeypress="return event.charCode >= 48 && event.charCode <= 57"
-                                                    name="quantity" 
-                                                    id="quantity" 
-                                                    class="form-control form-control-alternative" 
-                                                    placeholder="1" 
-                                                    value="1" 
-                                                    required 
-                                                    autofocus
-                                            >
+                                        <!--<input type="number" min="1" step="1" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="quantity" id="quantity" class="form-control form-control-alternative" placeholder="1" value="1" required autofocus>-->
                                     </div>
+
                                     <div class="quantity-btn">
+
                                         <div id="addToCart1">
-                                            <button class="btn btn-primary" v-on:click='addToCartAct'>{{ __('Add To Cart') }}</button>
+                                            <!--<button class="btn btn-primary" v-on:click='addToCartAct'>{{ __('Add To Cart') }}</button>-->
                                         </div>
                                     </div>
-                                   
+
                                 </div>
-                               @endif
+                                @endif
                                 <!-- Inform if closed -->
                                 @if (isset($openingTime)&&!empty($openingTime))
-                                        <span class="closed_time">{{__('Opens')}} {{ $openingTime }}</span>
-                                    @endif
+                                <span class="closed_time">{{__('Opens')}} {{ $openingTime }}</span>
+                                @endif
                                 <!-- End inform -->
                             </div>
                         </div>
@@ -83,19 +74,19 @@
                 <div class="card bg-secondary shadow border-0">
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="col-md-10 offset-md-1">
-                        <form role="form" method="post" action="{{ route('import.restaurants') }}" enctype="multipart/form-data">
-                            @csrf
-                            <div class="form-group text-center{{ $errors->has('item_image') ? ' has-danger' : '' }}">
-                                <label class="form-control-label" for="resto_excel">Import your file</label>
-                                <div class="text-center">
-                                    <input type="file" class="form-control form-control-file" name="resto_excel" accept=".csv, .ods, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" required>
+                            <form role="form" method="post" action="{{ route('import.restaurants') }}" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-group text-center{{ $errors->has('item_image') ? ' has-danger' : '' }}">
+                                    <label class="form-control-label" for="resto_excel">Import your file</label>
+                                    <div class="text-center">
+                                        <input type="file" class="form-control form-control-file" name="resto_excel" accept=".csv, .ods, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" required>
+                                    </div>
                                 </div>
-                            </div>
-                            <input name="category_id" id="category_id" type="hidden" required>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary my-4">{{ __('Save') }}</button>
-                            </div>
-                        </form>
+                                <input name="category_id" id="category_id" type="hidden" required>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary my-4">{{ __('Save') }}</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -105,10 +96,10 @@
 </div>
 @isset($restorant)
 <div class="modal fade" id="modal-restaurant-info" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
-    <div class="modal-dialog modal- modal-dialog-centered modal-lg" role="document" >
+    <div class="modal-dialog modal- modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 id="modalRestaurantTitle"  class="modal-title notranslate">{{ $restorant->name }}</h5>
+                <h5 id="modalRestaurantTitle" class="modal-title notranslate">{{ $restorant->name }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
@@ -120,7 +111,7 @@
                         <h4 class="heading mt-4">{{ $restorant->name }} &nbsp;@if(count($restorant->ratings))<span><i class="fa fa-star" style="color: #dc3545"></i> <strong>{{ number_format($restorant->averageRating, 1, '.', ',') }} <span class="small">/ 5 ({{ count($restorant->ratings) }})</strong></span></span>@endif</h4>
                         <p class="description">{{ $restorant->description }}</p>
                         @if(!empty($openingTime) && !empty($closingTime))
-                            <p class="description">{{ __('Open') }}: {{ $openingTime }} - {{ $closingTime }}</p>
+                        <p class="description">{{ __('Open') }}: {{ $openingTime }} - {{ $closingTime }}</p>
                         @endif
                     </div>
                     <div class="card-body">
@@ -140,7 +131,7 @@
                                     <div class="col-md-6">
                                         <h6 class="heading-small">{{ __('Phone') }}</h6>
                                         <p class="heading-small text-muted">{{ $restorant->phone }}</p>
-                                        <br/>
+                                        <br />
                                         <h6 class="heading-small">{{ __('Address') }}</h6>
                                         <p class="heading-small text-muted">{{ $restorant->address }}</p>
                                     </div>
@@ -153,21 +144,22 @@
                             </div>
                             <div class="tab-pane fade" id="tabs-icons-text-2" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
                                 @if(count($restorant->ratings) != 0)
-                                    <br/>
-                                    <h5>{{ count($restorant->ratings) }} {{ count($restorant->ratings) == 1 ? __('Review') : __('Reviews')}}</h5>
-                                    <hr />
-                                    
-                                    @foreach($restorant->ratings as $rating)
-                                        <div class="strip">
-                                            <span class="res_title"><b>{{ $rating->user->name }}</b></span><span class="float-right"><i class="fa fa-star" style="color: #dc3545"></i> <strong>{{ number_format($rating->rating, 1, '.', ',') }} <span class="small">/ 5</strong></span></span><br />
-                                            <span class="text-muted"> {{ $rating->created_at->format(env('DATETIME_DISPLAY_FORMAT','d M Y')) }}</span><br/>
-                                            <br/>
-                                            <span class="res_description text-muted">{{ $rating->comment }}</span><br />
-                                        </div>
-                                    @endforeach
+                                <br />
+                                <h5>{{ count($restorant->ratings) }} {{ count($restorant->ratings) == 1 ? __('Review') : __('Reviews')}}</h5>
+                                <hr />
+
+                                @foreach($restorant->ratings as $rating)
+                                <div class="strip">
+                                    <span class="res_title"><b>{{ $rating->user->name }}</b></span><span class="float-right"><i class="fa fa-star" style="color: #dc3545"></i> <strong>{{ number_format($rating->rating, 1, '.', ',') }} <span class="small">/ 5</strong></span></span><br />
+                                    <span class="text-muted"> {{ $rating->created_at->format(env('DATETIME_DISPLAY_FORMAT','d M Y')) }}</span><br />
+                                    <br />
+                                    <span class="res_description text-muted">{{ $rating->comment }}</span><br />
+                                </div>
+                                @endforeach
                                 @else
-                                  <p>{{ __('There are no reviews yet.') }}<p>
-                                @endif
+                                <p>{{ __('There are no reviews yet.') }}
+                                <p>
+                                    @endif
                             </div>
                         </div>
                     </div>
@@ -177,5 +169,3 @@
     </div>
 </div>
 @endisset
-
-
