ca320505b9462a119b65aa198ee4e8f7b151e164
iqbalbalte <iqbalbalte786@gmail.com>
2022-05-07 17:18:20 +0500

    latest changes template

--- resources/views/restorants/templates/templateThree.blade.php
+++ resources/views/restorants/templates/templateThree.blade.php
@@ -119,32 +119,6 @@ function clean($string)
     </div>
 </section>
 
-<section class="section pt-lg-0 mb--5 mt--9 d-none d-md-none d-lg-block d-lx-block">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="title white" <?php if ($restorant->description) {
-                                                echo 'style="border-bottom: 1px solid #f2f2f2;"';
-                                            } ?>>
-                    <h1 class="display-3 text-white notranslate" data-toggle="modal" data-target="#modal-restaurant-info" style="cursor: pointer;">{{ $restorant->name }}</h1>
-                    <p class="display-4" style="margin-top: 120px">{{ $restorant->description }}</p>
-
-                    <p><i class="ni ni-watch-time"></i> @if(!empty($openingTime))<span class="closed_time">{{__('Opens')}} {{ $openingTime }}</span>@endif @if(!empty($closingTime))<span class="opened_time">{{__('Opened until')}} {{ $closingTime }}</span> @endif | @if(!empty($restorant->address))<i class="ni ni-pin-3"></i></i> <a target="_blank" href="https://www.google.com/maps/search/?api=1&query={{ urlencode($restorant->address) }}"><span class="notranslate">{{ $restorant->address }}</span></a> | @endif @if(!empty($restorant->phone)) <i class="ni ni-mobile-button"></i> <a href="tel:{{$restorant->phone}}">{{ $restorant->phone }} </a> @endif</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-12">
-                @include('partials.flash')
-            </div>
-            @if (auth()->user()&&auth()->user()->hasRole('admin'))
-            @include('restorants.admininfo')
-            @endif
-        </div>
-    </div>
-
-</section>
 <section class="section section-lg d-md-block d-lg-none d-lx-none" style="padding-bottom: 0px">
     <div class="container">
         <div class="row">
